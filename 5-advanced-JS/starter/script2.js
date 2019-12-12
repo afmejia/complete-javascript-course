7b4e394d16bc7cf1281e5cfdf2953b1caf8f53c6
afmejia <afmejia23@utp.edu.co>
2019-12-12 07:10:23 -0500

    Borrowing methods

--- 5-advanced-JS/starter/script2.js
+++ 5-advanced-JS/starter/script2.js
@@ -15,16 +15,67 @@
 // }
 
 // Rewrite function using closures
-function interviewQuestion(job) {
-  return function(name) {
-    if (job === "designer") {
-      console.log(name + ", can you please explain what UX design is?");
-    } else if (job === "teacher") {
-      console.log("What subject do you teach, " + name + "?");
-    } else {
-      console.log("Hello " + name + ", what do you do?");
-    }
-  };
-}
+// function interviewQuestion(job) {
+//   return function(name) {
+//     if (job === "designer") {
+//       console.log(name + ", can you please explain what UX design is?");
+//     } else if (job === "teacher") {
+//       console.log("What subject do you teach, " + name + "?");
+//     } else {
+//       console.log("Hello " + name + ", what do you do?");
+//     }
+//   };
+// }
 
-interviewQuestion("teacher")("Felipe");
+// interviewQuestion("teacher")("Felipe");
+
+var john = {
+  name: "John",
+  age: 26,
+  job: "teacher",
+  presentation: function(style, timeOfDay) {
+    if (style === "formal") {
+      console.log(
+        "Good " +
+          timeOfDay +
+          ", Ladies and gentlemen! I'm " +
+          this.name +
+          " I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old. Have a nice " +
+          timeOfDay +
+          "."
+      );
+    } else if (style === "friendly") {
+      console.log(
+        "Hey! What's up? I'm " +
+          this.name +
+          " I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old. Have a nice " +
+          timeOfDay +
+          "."
+      );
+    }
+  }
+};
+
+var emily = {
+  name: "Emily",
+  age: 35,
+  job: "designer"
+};
+
+john.presentation("formal", "morning");
+
+john.presentation.call(emily, "friendly", "afternoon");
+
+// john.presentation.apply(emily, ["friendly", "afternoon"]);
+
+var johnFriendly = john.presentation.bind(john, "friendly");
+johnFriendly("morning");
+johnFriendly("night");
