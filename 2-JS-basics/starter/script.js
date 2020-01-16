bab42ffe5d17ab8541b58d7fbea9c110acbddf44
afmejia <afmejia23@utp.edu.co>
2020-01-16 06:41:47 -0500

    Reviewing

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,17 @@
-console.log("Hello World from Bogota!!!");
+var names = ["Peter", "Tony", "Chris"];
+// names[names.length] = "Felipe";
+// names.unshift("Andres");
+// console.log(names);
+// names.shift();
+// console.log(names);
+
+var john = {
+  firstName: "John",
+  lastName: "Smith",
+  birthYear: 1990,
+  family: ["Jane", "Mark", "Bob", "Emily"],
+  job: "teacher",
+  isMarried: true
+};
+
+console.log(john);
