d31e75ca8cd870a0139913b961e8526e9728a592
Felipe <felipe@elenas.la>
2020-05-08 07:18:04 -0500

    Building forkify recipe model

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,8 @@
+import Recipe from "./models/Recipe";
 import Search from "./models/Search";
+import { clearLoader, elements, renderLoader } from "./views/base";
+import * as recipeView from "./views/recipeView";
 import * as searchView from "./views/searchView";
-import { elements } from "./views/base";
 
 /** Global state of the app
  * - Search object
@@ -10,6 +12,9 @@ import { elements } from "./views/base";
  */
 const state = {};
 
+/**
+ * SEARCH CONTROLLER
+ */
 const controlSearch = async () => {
   // 1. Get query from view
   const query = searchView.getInput();
@@ -21,12 +26,56 @@ const controlSearch = async () => {
     // 3. Prepare UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
-    // 4. Search for recipes
-    await state.search.getResults();
+    try {
+      // 4. Search for recipes
+      await state.search.getResults();
 
-    // 5. Render results on UI
-    searchView.renderResults(state.search.results);
+      // 5. Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.results);
+    } catch (error) {
+      alert("Something went wrong");
+      clearLoader();
+    }
+  }
+};
+
+/**
+ * RECIPE CONTROLLER
+ */
+const controlRecipe = async () => {
+  // Get ID from URL
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+
+  if (id) {
+    // Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+
+    // Highlight selected search item
+    if (state.search) searchView.highlightSelected(id);
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+    try {
+      // Get recipe data and pase ingredients
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
+
+      // Calculate servings and time
+      state.recipe.calcServings();
+      state.recipe.calcTime();
+
+      // Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+    } catch (error) {
+      console.log(error);
+      alert("Error processing recipe");
+    }
   }
 };
 
@@ -34,3 +83,15 @@ elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
+elements.searchResPages.addEventListener("click", (e) => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.results, goToPage);
+  }
+});
+
+["hashchange", "load"].forEach((event) =>
+  window.addEventListener(event, controlRecipe)
+);
